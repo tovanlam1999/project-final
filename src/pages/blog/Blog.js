c604ecd6eebf6b88073fae616f6140614436ecdd
tovanlam1999 <tovanlam1999@gmail.com>
2023-11-25 09:33:18 +0700

    update 25/11

---
+++ src/pages/blog/Blog.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import BlogBody from '../../component-blog/blogbody/BlogBody';
+import Footer from '../../components/footer/Footer';
+import Header from '../../components/header/Header';
+import Navbar from '../../components/navbar/Navbar';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+export default function Blog() {
+    const [animal, setAnimal] = useState();
+    const { id } = useParams();
+
+    const getAnimal = () => {
+        const api = `https://6560124683aba11d99d02b5c.mockapi.io/animalBlog`;
+
+        axios
+            .get(api)
+            .then((res) => {
+                setAnimal(res.data);
+            })
+            .catch((Error) => {
+                console.log(Error);
+            });
+    };
+
+    useEffect(() => {
+        getAnimal();
+    }, []);
+
+    return (
+        <div className="Blog">
+            <Header />
+            <Navbar />
+            <BlogBody animal = {animal} />
+            <Footer />
+        </div>
+    );
+}
